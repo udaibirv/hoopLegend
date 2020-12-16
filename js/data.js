ca3a245e0e2151cd8b23ac86fb5275a45ba15bb8
udaibirv <udaibirv@gmail.com>
2020-12-15 20:02:05 -0800

    work in progress for: Create a clean form that includes inputs for user's name, favorite team, and e-mail. #1

--- js/data.js
+++ js/data.js
@@ -1,1 +1,27 @@
 /* exported data */
+var data = {
+  profile: {
+    name: '',
+    favTeam: '',
+    email: ''
+  }
+}
+
+window.addEventListener('beforeunload', function(e){
+  var stringData = JSON.stringify(data);
+  localStorage.setItem('hoop-legends', stringData);
+})
+
+var returnData = localStorage.getItem('hoop-legends');
+if(returnData !== null){
+  data = JSON.parse(returnData);
+}
+
+function request(){
+var xhr = new XMLHttpRequest();
+xhr.open('GET','https://www.balldontlie.io/api/v1/games');
+xhr.responseType = 'json';
+
+  xhr.send();
+console.log(xhr);
+}
---
+++ js/main.js
@@ -0,0 +1,11 @@
+var $submit = document.querySelector('.submit-button');
+var $form = document.querySelector('form');
+
+$form.addEventListener('submit', function(e){
+  e.preventDefault();
+  data.profile.name = $form.elements['name'].value;
+  data.profile.favTeam = $form.elements['favTeam'].value;
+  data.profile.email = $form.elements['email'].value;
+  console.log(data);
+  request();
+})
