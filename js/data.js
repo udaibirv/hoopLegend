8a7b33b815f8c7da03964bb13d06dcae66445f07
udaibirv <udaibirv@gmail.com>
2020-12-16 17:03:36 -0800

    get Data of the team that user selected in the first page.#2

--- js/data.js
+++ js/data.js
@@ -16,3 +16,16 @@ var returnData = localStorage.getItem('hoop-legends');
 if(returnData !== null){
   data = JSON.parse(returnData);
 }
+
+var xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://www.balldontlie.io/api/v1/teams');
+xhr.responseType = 'json';
+xhr.addEventListener('load', function(e){
+
+  for(var i = 0; i < xhr.response.data.length; i++){
+    if(data.profile.favTeam === xhr.response.data[i]["full_name"]){
+      $scoreHeader.textContent = 'Recent results for ' + xhr.response.data[i]["full_name"];
+    }
+  }
+})
+xhr.send();
