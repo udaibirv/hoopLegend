fd37b33df1eeeac99f6a878f307a1c773fd84d6b
udaibirv <udaibirv@gmail.com>
2020-12-17 17:08:58 -0800

    Show the team name at the top of the page.#2

--- js/data.js
+++ js/data.js
@@ -24,26 +24,8 @@ xhr.addEventListener('load', function(e){
 
   for(var i = 0; i < xhr.response.data.length; i++){
     if(data.profile.favTeam === xhr.response.data[i]["full_name"]){
-      $scoreHeader.textContent = 'Recent results for ' + xhr.response.data[i]["full_name"];
+      $scoreHeader.textContent = 'Recent results for the ' + xhr.response.data[i]["full_name"];
     }
   }
 })
 xhr.send();
-
-
-  var game = new XMLHttpRequest();
-  game.open('GET', "https://www.balldontlie.io/api/v1/games");
-  game.responseType = 'json';
-  game.addEventListener('load', function(e){
-    var gamesArray = [];
-    for(var i = 0; i < game.response.data.length; i++){
-    gamesArray.push(game.response.data[i]);
-      if(data.profile.favTeam === game.response.data[i]["home_team"].full_name){
-        $homeScore.textContent = 'Boston lost the game ' + game.response.data[i]["home_team_score"] + ' to ' + game.response.data[i]["visitor_team_score"];
-        gamesArray.push(game.response.data[i]["home_team"]);
-        console.log(gamesArray);
-    }
-
-  }
-  })
-  game.send();
